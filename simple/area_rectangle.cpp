15c80f55b32537612f5fdc552d5346992615820e
Ankush <psrankush@gmail.com>
2025-04-23 22:13:24 +0530

    basic is done

---
+++ simple/area_rectangle.cpp
@@ -0,0 +1,25 @@
+// QUESTION 2: WRITE AN PROGRAM TO PRINT AND FIND OUT THE 
+// AREA AND PARIMETER OF A RECTANGLE
+
+// intialize the header file
+#include<iostream>
+using namespace std;
+
+// main file
+int main()
+{
+    // Take a data type integer & L for length of a Rectangle & B for breath of a Rectangle.
+    int l,b;    
+
+    // This is simple line to pring what we find?
+
+    cout<<"Find the area & parimeter of a rectangle"<<endl;
+        
+    // Take a input length of a rectngle.
+    cout<<"enter the length of a rectangle: ";
+        cin>>l;
+
+    //Take a input breath of a rectangle. 
+        cout<<"enter the length of a rectangle: ";
+        cin>>l;
+}
