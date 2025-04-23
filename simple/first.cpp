45f41753958b1b035f975413b6e517a8df1a1ad6
Ankush <psrankush@gmail.com>
2025-04-23 22:00:59 +0530

    firt program is done

---
+++ simple/first.cpp
@@ -0,0 +1,12 @@
+// QUESTION 1: WRITE A PROGRAM TO PRINT HELLO WORLD
+
+// INITIALIZE THE HEADER FILE
+#include<iostream>
+using namespace std;
+
+// MAIN FILE
+int main()
+{
+    // print a hello world.
+    cout<<"Hello World";
+}
