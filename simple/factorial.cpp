238259f9b33f09dec5cb7029dc8a5c929e1a04c9
Ankush <psrankush@gmail.com>
2025-04-25 21:55:22 +0530

    next program will be done soon

---
+++ simple/factorial.cpp
@@ -0,0 +1,28 @@
+// QUESTION : WRITE A PROGRAM IN C++ TO FIND OUT THE FACTORIAL OF A NUMBER
+
+// CODE
+
+// header file
+#include<iostream>
+using namespace std;
+
+// main file
+int main()
+{
+    // take a vairable in integer i is for loop and n is number 
+    int fact=1,i,n;
+
+    // this line for print and take input of a number
+    cout<<"Enter the value: ";
+    cin>>n;
+
+    // loop start to take start with 1 and go to number value 
+    for(i=0;i<=n;i++)
+    {
+        // fact means factorial,so this formula helps to find out the value 
+        fact*=i;
+    }
+
+    
+    cout<<"The Factorial of a Number is "<<fact;
+}
