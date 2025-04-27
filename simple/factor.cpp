3cf6cf45d25cd06b1f69808d1bdcc34f79a57b66
Ankush <psrankush@gmail.com>
2025-04-27 21:30:18 +0530

    code is updated

--- simple/factor.cpp
+++ simple/factor.cpp
@@ -7,5 +7,11 @@ using namespace std;
 // main file
 int main()
 {
-    
+    // take variable for input a number 
+    int n;
+
+    cout<<"Enter your number: ";
+    cin>>n;
+
+    // start a loop of check variable one by one 
 }
