2b55eadfb2caecdd703dec36ee12ec5333125084
Ankush <psrankush@gmail.com>
2025-04-28 07:44:24 +0530

    function is calling

---
+++ Array/number_function.cpp
@@ -0,0 +1,81 @@
+/*QUESTION: Write a program in c++ with the help of functions and array to find out the greatest,lowest and sorting 
+in an array*/
+
+// header file
+#include<iostream>
+using namespace std;
+
+// functions call
+void greatest(int []);
+void lowest(int []);
+void sorting(int []);
+
+// main file
+int main()
+{
+    // take an array with 10 size and i for loops
+    int a[10],i;
+
+    for(i=0;i<10;i++)
+    {
+        cout<<"Enter Your Array No "<<i<<" :";
+        cin>>a[i];
+    }
+
+    greatest(a);
+
+    lowest(a);
+
+    sorting(a);
+
+}
+
+void greatest(int no[10])
+{
+    int i,gr=0;
+
+    for(i=0;i<10;i++)
+    {
+        if(no[i]>gr)
+        gr=no[i];
+
+    }
+    cout<<"Greteast No.: "<<gr;
+    }
+
+void lowest(int no[10])
+{
+        int i,low=no[0];
+
+        for(i=0;i<10;i++)
+        {
+            if(no[i]<low)
+            low=no[i];
+        }
+        cout<<"\nLowest No in an array: "<<low;
+}
+
+void sorting(int no[10])
+{
+    int i,j;
+    int temp=0;
+
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            if(no[i]<no[j])
+          {  temp=no[i];
+            no[i]=no[j];
+            no[j]=temp;
+        }
+    }
+    }
+
+cout<<"\nSorting:";
+    for(i=0;i<10;i++)
+    {
+        
+        cout<<no[i]<<"\t";
+    }
+}
