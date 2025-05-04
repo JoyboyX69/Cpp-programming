f4b95d0d5441cf84fe854543b5de896ca278263b
Ankush <psrankush@gmail.com>
2025-05-04 22:35:06 +0530

    Q10 is complete

--- patterns/Q10.cpp
+++ patterns/Q10.cpp
@@ -1,22 +1,39 @@
+/*QUESTION: Write a program in c++ to print the same pattern is given below
+*
+**
+***
+****
+*****
+*/
+
+// header file
 #include<iostream>
 using namespace std;
 
+// main file
 int main()
 {
+    // take a four variable, first is input and 3 for loops to print the pattern
     int n,i,j,s;
 
+    // input your value
     cout<<"Enter a number: ";
     cin>>n;
 
+    // first loop is start 
     for(i=1;i<=n;i++)
     {
+        // print next line to each and every step
         cout<<endl;
 
+        // second loop is start for gap 
         for(s=n-1;s>=i;s--)
         cout<<" ";
 
+        // third loop is start to print stars 
     for(j=1;j<=i;j++)
     cout<<"*";
     }
     return 0;
+    // end of code
 }
