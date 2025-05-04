51d3a757df81e16fef60a03bef33685eb0fa59cc
Ankush <psrankush@gmail.com>
2025-05-04 22:15:48 +0530

    Q7 is already donr

--- patterns/Q7.cpp
+++ patterns/Q7.cpp
@@ -1,19 +1,38 @@
+/*QUESTION: Write a program in c++ to print this item
+*
+**
+***
+****
+*****
+*/
+
+// header file
 #include<iostream>
 using namespace std;
 
+// main file
 int main()
 {
+    // take a three variable first for input and 2 for loops to print this patterns
     int n,i,j;
 
+    // input your value
     cout<<"Enter an number: ";
     cin>>n;
 
+    // first loop is start
     for(i=1;i<=n;i++)
     {
+        // print the new line
         cout<<endl;
 
+        // second loop start 
         for(j=1;j<=i;j++)
+
+        // print the star pattern
         cout<<"*";
     }
     return 0;
+
+    // end of code
 }
