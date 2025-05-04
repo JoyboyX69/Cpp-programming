a3c2a8fa5d13b53ff7564b4f69fd039e89281611
Ankush <psrankush@gmail.com>
2025-05-04 22:30:20 +0530

    Q9 is complete too,logic for pattern is more clear

--- patterns/Q9.cpp
+++ patterns/Q9.cpp
@@ -1,22 +1,39 @@
+/*QUESTION: Write a program in c++ to print the same pattern
+1
+23
+456
+78910
+*/
+
+// header file
 #include<iostream>
 using namespace std;
 
+// main file
 int main()
 {
+    // take 4 variable,first is input and 2 for loops and one for increment the value
     int n,i,j,a=1;
 
+    // input your number
     cout<<"Enter a number: ";
     cin>>n;
 
+    // first loop is start
     for(i=1;i<=n;i++)
     {
+        // print the next line
         cout<<endl;
 
+        // second loop is start
         for(j=1;j<=i;j++)
-        {cout<<a;
-        a++;
+        {
+            // print the a variable to print the same pattern
+            cout<<a;
+            a++;
         }
  
     }
     return 0;
+    // end of code
 }
