0e9f349e54d1abc25e3ffef5cf998a679b7fcf11
Ankush <psrankush@gmail.com>
2025-05-04 22:23:08 +0530

    Q8 is complete to print the pattern

--- patterns/Q8.cpp
+++ patterns/Q8.cpp
@@ -1,21 +1,38 @@
+/*QUESTION : Write a program in c++ to print this pattern
+1
+22
+333
+4444
+55555
+*/
+
+// header file
 #include<iostream>
 using namespace std;
 
+// main file
 int main()
 {
+    // take a 3 variable, first is input and another 2 for loops to print the pattern
     int n,i,j;
 
+    // input your value
     cout<<"Enter a number: ";
     cin>>n;
 
+    // first loop is start
     for(i=1;i<=n;i++)
     {
+        // print the next line
         cout<<endl;
 
+        // second loop is start
         for(j=1;j<=i;j++)
         {
+            // print the iteration of first loop (i).
             cout<<i;
         }
     }
     return 0;
+    // end of code
 }
