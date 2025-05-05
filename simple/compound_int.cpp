e8cc513aaa7556ec3335a25bcad4e04dee6005f5
Ankush <psrankush@gmail.com>
2025-05-05 17:32:03 +0530

    code is ready for use

--- simple/compound_int.cpp
+++ simple/compound_int.cpp
@@ -16,16 +16,23 @@ int main()
     cout<<"Enter The Rate: ";
     cin>>r;
     cout<<"Enter The Time: ";
-    cin>>t;
+    cin>>t
+
+    // start a loop to calculate the time period
         for(i=1;i<=t;i++)
         {
+            // amount will be calculate
             amount*=(1+r/100);
         }
+        // store the value of p in amount
         amount*=p;
+        
+        // amount is substract Principal to calculate the compound interest
         ci=amount-p;
+        // print the value of compound and amount
         cout<<"compound interest value is "<<ci;
         cout<<"\nAmount="<<amount;
 
         return 0;
-            
+            // end of code
 }
