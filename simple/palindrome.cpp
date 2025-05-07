32ddabb0141de6936361e7f3a629849c6e1dfd0d
Ankush <psrankush@gmail.com>
2025-05-07 11:16:39 +0530

    palindrome program will be done

--- simple/palindrome.cpp
+++ simple/palindrome.cpp
@@ -7,5 +7,30 @@ using namespace std;
 // main file
 int main()
 {
-    
+  /**/  take 4 variable to first for number input,second for temp variable,res reverse the value
+    and fourth for store the reverse value*/
+    int n,temp,res,reverse;
+
+    // input your value
+    cout<<"Enter your number: ";
+    cin>>in;
+
+    // start a while loop for reverse the number
+    while(n!=0)
+    {
+        // this are the condition helps to reverse the number
+        res=n%10;
+        reverse=reverse*10+res;
+        n/=10;
+    }
+    // if reverse are same as temp then print palindrome number
+    if(reverse==temp)
+    cout<<"This is a palindrome number";
+
+    // if not same then print not a palindrome number
+    else 
+    cout<<"This is not a palindrome number;"
+
+    return 0;
+    // end of code
 }
