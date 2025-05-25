be27baf93d19ea3c368b8cce7d7b31d08beb1f0b
Ankush <psrankush@gmail.com>
2025-05-25 22:43:58 +0530

    Program is donet

--- simple/swap.cpp
+++ simple/swap.cpp
@@ -5,3 +5,20 @@ QUESTION: Write a program to swap the value with extra variable in c++
 using namespace std;
 
 // main file
+int main()
+{
+    int a,b,temp;
+
+    cout<<"Enter the value of A: ";
+    cin>>a;
+
+    cout<<"Enter the value of B: ";
+    cin>>b;
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    cout<<"Swap value is ";
+    cout<<"A="<<a<<" "<<"B="<<b;
+}
