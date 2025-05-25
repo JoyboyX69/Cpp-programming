76b1388b5cc234c1686fd03a9abc9ee42735f5c1
Ankush <psrankush@gmail.com>
2025-05-25 22:46:03 +0530

    comment are added to this program

--- simple/swap.cpp
+++ simple/swap.cpp
@@ -7,18 +7,27 @@ using namespace std;
 // main file
 int main()
 {
+    // take a variable for input and temp for swap
     int a,b,temp;
 
+    // input of a
     cout<<"Enter the value of A: ";
     cin>>a;
 
+    // input of b
     cout<<"Enter the value of B: ";
     cin>>b;
 
+    // swap program logic
     temp=a;
     a=b;
     b=temp;
 
+    // print the swap value
     cout<<"Swap value is ";
     cout<<"A="<<a<<" "<<"B="<<b;
+
+    return 0;
+
+    // end of code
 }
