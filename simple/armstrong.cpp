469886304738563a37c8bbe6a2695b070b91ea2c
Ankush <psrankush@gmail.com>
2025-05-25 21:19:11 +0530

    start a program armstrong number

---
+++ simple/armstrong.cpp
@@ -0,0 +1,21 @@
+// QUESTION: Write a program to check armstrong number or not in c++
+
+// header file
+#include<iostream>
+using namespace std;
+
+// main file
+int main()
+{
+    // Take a variable to do check armstrong number
+    int n,r,sum=0,temp;
+
+    // take a input 
+    cout<<"Enter your term: ";
+    cin>>n;
+
+    // save a input number to temp value
+    temp=n;
+    
+    // start a loop to check armstrong number
+}
